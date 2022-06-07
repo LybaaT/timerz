932279367426c5a4d8d8fed2a5b52a64dc53c738
Lyba T <105508273+LybaaT@users.noreply.github.com>
2022-06-08 03:12:48 +0500

    DONEE

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
-var counter = 1000; 
+var counter = 1800; 
 
 function showNotification() {
-    const notification = new Notification("Laiba says", {
-        body: "oiiii",
+    const notification = new Notification("2COOL4U ALERT!!!!", {
+        body: "Laiba wishes to remind u that she is always 2 cool 4 u. ur my favorite loser. see u in 15 minutes :)",
+        icon: "icon.png"
     });
 }
 
@@ -17,7 +18,7 @@ if (counter >= 0) {
 
 if (Notification.permission == "granted" && counter <= 0) {
     showNotification();
-    counter = 1000
+    counter = 1800
 }
 else if (Notification.permission !== "denied") {
     Notification.requestPermission().then(permission => {
