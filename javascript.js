a9c294fd173192d70a40a13c0288330152d24f46
Lyba T <105508273+LybaaT@users.noreply.github.com>
2022-06-07 20:15:30 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+var counter = 10; 
+
+setInterval( function() {
+counter--;
+
+if (counter >= 0) {
+    document.getElementById("count").innerHTML = counter;
+    }
+}, 1000);
+
+function showNotification() {
+    const notification = new Notification("New message", {
+        body: "oiiii",
+    });
+}
+
+console.log(Notification.permission);
+
+if (Notification.permission == "granted" && counter >= 0) {
+    showNotification();
+}
+else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(permission => {
+        console.log(permission);
+    });
+}
